4b6c2f2e5a822a80599c4fc507ad4cb3c78a9bdc
Jeanluca <jeanlucafp@gmail.com>
2024-11-08 11:31:32 -0300

    withdraw & donate

--- crowdfunding/programs/crowdfunding/src/lib.rs
+++ crowdfunding/programs/crowdfunding/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
+use anchor_lang::solana_program::system_instruction;
 
 declare_id!("82o3jdKmymbQBoN3m2dL3PGfL84UFmqa68a8AtjHM7Dc");
 
@@ -13,6 +14,41 @@ pub mod crowdfunding {
         campaign.desc = desc;
         campaign.amount_donated = 0;
         campaign.owner = *ctx.accounts.user.key;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut &ctx.accounts.user;
+
+        if campaign.owner != *user.key {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+
+        if **campaign.to_account_info().lamports.borrow() - rent_balance < amount {
+            return Err(ProgramError::InsufficientFunds);
+        }
+
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **campaign.to_account_info().try_borrow_mut_lamports()? += amount;
+        
+        Ok(())
+    }
+
+    pub fn donate(ctx: Context<Donate>, amount: u64) -> ProgramResult {
+        let ix = system_instruction::transfer(
+            &ctx.accounts.user.key(),
+            &ctx.accounts.campaign.key(),
+            amount,
+        );
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.user.to_account_info(),
+                ctx.accounts.campaign.to_account_info(),
+            ],
+        )?;
         Ok(())
     }
 }
@@ -32,4 +68,21 @@ pub struct Campaign {
     pub desc: String,
     pub amount_donated: u64,
     pub owner: Pubkey,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info>{
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct Donate<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    pub system_program: Program<'info, System>,
 }
