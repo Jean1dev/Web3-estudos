23630ae0586f77d37f5d930f5bc66bee4175ab45
Jeanluca <jeanlucafp@gmail.com>
2024-11-21 18:00:04 -0300

    feat/create_caixinha

--- caixinha-dapp/programs/caixinha-dapp/src/lib.rs
+++ caixinha-dapp/programs/caixinha-dapp/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("Do72vP6pm2g9UDAy8mhP7LrEPoHD4c5SQXacbrQ2aoDv");
 
@@ -6,11 +7,36 @@ declare_id!("Do72vP6pm2g9UDAy8mhP7LrEPoHD4c5SQXacbrQ2aoDv");
 pub mod caixinha_dapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_caixinha(
+        ctx: Context<CreateCaixinha>,
+        name: String,
+        desc: String,
+        ref_id: String,
+    ) -> ProgramResult {
+        let caixinha = &mut ctx.accounts.caixinha;
+        caixinha.name = name;
+        caixinha.desc = desc;
+        caixinha.ref_id = ref_id;
+        caixinha.amount = 0;
+        caixinha.owner = *ctx.accounts.user.key;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateCaixinha<'info> {
+    #[account(init, payer = user, space = 8 + 8 + 8 + 32 + 32, seeds=[b"CAIXINHA_DEMO".as_ref(), user.key().as_ref()], bump)]
+    pub caixinha: Account<'info, Caixinha>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Caixinha {
+    pub name: String,
+    pub desc: String,
+    pub ref_id: String,
+    pub amount: u64,
+    pub owner: Pubkey,
+}
--- caixinha-dapp/tests/caixinha-dapp.ts
+++ caixinha-dapp/tests/caixinha-dapp.ts
@@ -1,15 +1,32 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { CaixinhaDapp } from "../target/types/caixinha_dapp";
+import { expect } from "chai";
 
 describe("caixinha-dapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.CaixinhaDapp as Program<CaixinhaDapp>;
+  const caixinhaKeyPair = anchor.web3.Keypair.generate();
 
-  const program = anchor.workspace.CaixinhaDapp as Program<any>;
+  it("Creates a new caixinha!", async () => {
+    const txHash = await program.methods.createCaixinha(
+      "Test Name",
+      "Test Description",
+      "646f538de5cd54cc6344ec69"
+    )
+    .accounts({
+      //@ts-ignore
+      caixinha: caixinhaKeyPair.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([caixinhaKeyPair])
+    .rpc();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log("txHash", txHash);
+
+    const caixinha = await program.account.caixinha.fetch(caixinhaKeyPair.publicKey);
+    expect(caixinha.name).to.eq("Test Name");
   });
 });
