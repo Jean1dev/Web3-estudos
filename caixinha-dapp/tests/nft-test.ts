d46b73112eee63f40fc735399f9888605e2ef0d3
Jeanluca <jeanlucafp@gmail.com>
2025-03-21 08:26:57 -0300

    Update nft-test.ts

--- caixinha-dapp/tests/nft-test.ts
+++ caixinha-dapp/tests/nft-test.ts
@@ -4,33 +4,33 @@ import { CaixinhaDapp } from "../target/types/caixinha_dapp";
 import { expect } from "chai";
 
 describe("nft tests", () => {
-    const provider = anchor.AnchorProvider.local();
-    anchor.setProvider(provider);
-    const program = anchor.workspace.CaixinhaDapp as Program<CaixinhaDapp>;
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.CaixinhaDapp as Program<CaixinhaDapp>;
 
-    it("initialize nft", async () => {
-        const newAccountKp = new web3.Keypair();
+  it("initialize nft", async () => {
+    const newAccountKp = new web3.Keypair();
 
-        const txHash = await pg.program.methods
-            .createNewNft("nft criado no test", "img", 1.2)
-            .accounts({
-                nft: newAccountKp.publicKey,
-                authority: pg.wallet.keypair,
-                systemProgram: pg.PROGRAM_ID,
-            })
-            .signers([pg.wallet.keypair])
-            .rpc();
+    const txHash = await pg.program.methods
+      .createNewNft("nft criado no test", "img", 1.2)
+      .accounts({
+        nft: newAccountKp.publicKey,
+        authority: pg.wallet.keypair,
+        systemProgram: pg.PROGRAM_ID,
+      })
+      .signers([pg.wallet.keypair])
+      .rpc();
 
-        console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
+    console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
 
-        // Confirm transaction
-        await pg.connection.confirmTransaction(txHash);
+    // Confirm transaction
+    await pg.connection.confirmTransaction(txHash);
 
-        // Fetch the created account
-        const newAccount = await pg.program.account.myAchievement.fetch(
-            newAccountKp.publicKey
-        );
+    // Fetch the created account
+    const newAccount = await pg.program.account.myAchievement.fetch(
+      newAccountKp.publicKey
+    );
 
-        console.log("On-chain data is:", newAccount);
-    });
-})
+    console.log("On-chain data is:", newAccount);
+  });
+});
